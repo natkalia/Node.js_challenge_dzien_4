78dedae8458a5c01803fd8deb28954a65af6e7c0
natkalia <natalia.banasiewicz@wp.pl>
2019-08-12 10:36:35 +0200

    exercise zadanieDnia2 solved

--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -1,1 +1,17 @@
-//Twój kod
+//Spróbuj zmodyfikować powyższy kod w taki sposób, aby zwracać przeglądarce tekst na temat jej samej.
+
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+  
+  const browser = req.headers['user-agent'];
+  
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.end(browser);
+
+});
+
+srv.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
+
