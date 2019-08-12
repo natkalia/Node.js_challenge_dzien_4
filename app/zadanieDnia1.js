86ac90f6df7945069001fc1fe55dbabc5b474536
natkalia <natalia.banasiewicz@wp.pl>
2019-08-12 10:27:20 +0200

    exercise zadanieDnia1 solved

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,15 @@
-//Twój kod
+// Stwórz back-end, który wyświetli w przeglądarce użytkownika po wejściu np. na localhost:3000 napis Hello, World from back-end!.
+
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+  
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.end('Hello, World from back-end!');
+
+});
+
+srv.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
+
